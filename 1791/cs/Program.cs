4fde9c772781cee8e28eda9bceffb5b0585c550e
wrnlb666 <wrnlb666@gmail.com>
2024-06-27 21:09:41 +0800

    1791: cs - attempt 1

---
+++ 1791/cs/Program.cs
@@ -0,0 +1,10 @@
+﻿int[][] edges = [[1,2],[2,3],[4,2]];
+int res = new Solution().FindCenter(edges);
+Console.WriteLine(res);
+
+
+public class Solution {
+    public int FindCenter(int[][] edges) {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+}
