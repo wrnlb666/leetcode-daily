3f66d321fdf0fa7a609f5eebd0ac4b913402f2e1
wrnlb666 <wrnlb666@gmail.com>
2024-06-28 22:49:31 +0800

    2285: cs - attempt 1

---
+++ 2285/cs/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = 5;
+int[][] roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]];
+long res = new Solution().MaximumImportance(n, roads);
+Console.WriteLine(res);
+
+
+public class Solution {
+    public long MaximumImportance(int n, int[][] roads) {
+        long[] count = new long[n];
+        foreach (int[] road in roads) {
+            foreach (int v in road) {
+                count[v] += 1;
+            }
+        }
+
+        long res = 0;
+        Array.Sort(count);
+        for (long i = 0; i < count.Length; i++) {
+            res += count[i] * (i + 1);
+        }
+
+        return res;
+    }
+}
