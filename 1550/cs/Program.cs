efd28eab2b7543e03eb44c17e17ddf7c9b260a36
wrnlb666 <wrnlb666@gmail.com>
2024-07-02 00:18:18 +0800

    1550: cs - attempt 1

---
+++ 1550/cs/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = [2,6,4,1];
+bool res = new Solution().ThreeConsecutiveOdds(arr);
+Console.WriteLine(res);
+
+
+public class Solution {
+    public bool ThreeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        foreach (int x in arr) {
+            if (x % 2 == 0) {
+                count = 0;
+            } else {
+                count += 1;
+                if (count == 3) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
