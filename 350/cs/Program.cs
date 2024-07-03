175bf74c96096acebbf32ae462818f3b02a2529c
wrnlb666 <wrnlb666@gmail.com>
2024-07-03 23:51:19 +0800

    350

---
+++ 350/cs/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] nums1 = [1,2,2,1];
+int[] nums2 = [2,2];
+int[] res = new Solution().Intersect(nums1, nums2);
+Console.WriteLine(String.Join(" ", res));
+
+
+public class Solution {
+    public int[] Intersect(int[] nums1, int[] nums2) {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+        int i1 = 0;
+        int i2 = 0;
+        List<int> res = new();
+
+        for (;i1 < nums1.Length && i2 < nums2.Length;) {
+            if (nums1[i1] == nums2[i2]) {
+                res.Add(nums1[i1]);
+                i1++;
+                i2++;
+            } else if (nums1[i1] < nums2[i2]) {
+                i1++;
+            } else {
+                i2++;
+            }
+        }
+
+        return res.ToArray();
+    }
+}
