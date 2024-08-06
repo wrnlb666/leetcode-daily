a28955b6b8c02084f7cf2134664d88c15aafa5ae
wrnlb666 <wrnlb666@gmail.com>
2024-08-06 16:34:02 +0800

    2053: cs - attempt 1

---
+++ 2053/cs/Program.cs
@@ -0,0 +1,30 @@
+﻿string[] arr = ["d","b","c","b","c","a"];
+int k = 2;
+string res = new Solution().KthDistinct(arr, k);
+Console.WriteLine(res);
+
+
+public class Solution {
+    public string KthDistinct(string[] arr, int k) {
+        Dictionary<string, int> dict = new();
+        for (int i = 0; i < arr.Length; i++) {
+            string s =  arr[i];
+            if (dict.ContainsKey(s)) {
+                dict[s] = -1;
+            } else {
+                dict[s] = i;
+            }
+        }
+
+        foreach (string s in arr) {
+            if (dict[s] != -1) {
+                k -= 1;
+            }
+            if (k == 0) {
+                return s;
+            }
+        }
+
+        return "";
+    }
+}
