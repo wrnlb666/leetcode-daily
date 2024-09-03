7b1064bca9e90c26e54c90c83d192488a7d7da96
wrnlb666 <wrnlb666@gmail.com>
2024-09-03 12:49:52 +1000

    1945: ts - attempt 1 (I have no idea why this approach is slower than string manipulation)

---
+++ 1945/ts/wrnlb.ts
@@ -0,0 +1,29 @@
+function getLucky(s: string, k: number): number {
+    let sum: number = 0
+    for (let i = 0; i < s.length; i++) {
+        let num = s.charCodeAt(i) - 96
+        if (num < 10) {
+            sum += num
+        } else {
+            sum += Math.floor(num / 10)
+            sum += num % 10
+        }
+    }
+    for (let c = 1; c < k; c++) {
+        let newSum = 0
+        while (sum > 0) {
+            newSum += sum % 10
+            sum = Math.floor(sum / 10)
+        }
+        sum = newSum
+    }
+    return sum
+}
+
+
+(() => {
+    const s: string = "zbax"
+    const k: number = 2
+    const res: number = getLucky(s, k)
+    console.log(res)
+})()
