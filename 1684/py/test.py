bd9f043289c6de4cd05750dbf7bbbbfd5522712a
wrnlb666 <wrnlb666@gmail.com>
2024-09-12 11:22:00 +1000

    1684: py3 - attempt 2

--- 1684/py/test.py
+++ 1684/py/test.py
@@ -1,13 +1,15 @@
-from typing import List, Set
+from typing import List
 
 
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        s: Set[str] = set(x for x in allowed)
+        s: List[bool] = [False] * 26
         res: int = 0
+        for c in allowed:
+            s[ord(c) - 0x61] = True
         for word in words:
             for c in word:
-                if c not in s:
+                if not s[ord(c) - 0x61]:
                     break
             else:
                 res += 1
