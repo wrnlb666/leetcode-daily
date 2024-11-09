eb3140bb5f1c17b85bf40184c19d37b3a20311fb
wrnlb666 <wrnlb666@gmail.com>
2024-11-09 18:32:35 +1100

    3133: go - attempt 3

--- 3133/go/main.go
+++ 3133/go/main.go
@@ -7,26 +7,45 @@ import (
 
 
 func minEnd(n int, x int) int64 {
-	index := 0
-	n -= 1
-	for n > 0 {
-		current := n & 1
-		for x & (1 << index) > 0 {
-			index++
-		}
-		if current == 1 {
-			x = x | (1 << index)
-		}
-		index++
-		n >>= 1
-	}
-	return int64(x)
+    n -= 1
+    if n == 0 {
+        return int64(x)
+    }
+
+    cycle := []int{}
+    end := x + 1
+    total := 0
+    last := x
+    for range n {
+        newInt := (last + 1) | x
+        gap := newInt - last
+        total += gap
+        cycle = append(cycle, gap)
+        last = newInt
+        if gap == end {
+            break
+        }
+    }
+
+    if n == len(cycle) {
+        return int64(last)
+    }
+
+    res := int64(x)
+    quotient := n / len(cycle)
+    res += int64(quotient * total)
+    remainder := n % len(cycle)
+    for i := range remainder {
+        res += int64(cycle[i])
+    }
+
+    return res
 }
 
 
 func main() {
     n := 3
-    x := 4
+    x := 1
 
     res := minEnd(n, x)
     fmt.Println(res)
