37e54f9394354bb34e696054e9367e3584f356ea
wrnlb666 <wrnlb666@gmail.com>
2024-11-30 16:31:59 +1100

    2097: py3 - attempt 2

--- 2097/py/test.py
+++ 2097/py/test.py
@@ -1,41 +1,43 @@
-from typing import List, Dict, Counter
-from collections import defaultdict
+from typing import Deque, List, Dict
+from collections import defaultdict, deque
 
 
 class Solution:
     def validArrangement(self, pairs: List[List[int]]) -> List[List[int]]:
-        cr: Dict[int, List[int]] = defaultdict(list)
-        cl: Counter[int] = Counter()
-        for i in range(len(pairs)):
-            s, e = pairs[i]
-            cr[s].append(e)
-            cl[e] += 1
-        mid: List[int] = pairs[-1]
-        for s, e in cr.items():
-            if cl[s] < len(e):
-                if len(e) == 1:
-                    mid = [s, e[0]]
-                    break
-                for n in e:
-                    if cl[n] <= len(cr[n]):
-                        mid = [s, n]
-                        break
-        right = [mid]
-        curr: int = mid[1]
-        
-        cr[mid[0]].remove(mid[1])
-        if not cr[mid[0]]:
-            del cr[mid[0]]
+        adj_set: Dict[int, Deque[int]] = defaultdict(deque)
+        in_degree: Dict[int, int] = defaultdict(int)
+        out_degree: Dict[int, int] = defaultdict(int)
 
-        while True:
-            if curr not in cr:
+        for (s, e) in pairs:
+            adj_set[s].append(e)
+            out_degree[s] += 1
+            in_degree[e] += 1
+
+        res: List[int] = list()
+
+        def visit(node: int):
+            while adj_set[node]:
+                next: int = adj_set[node].popleft()
+                visit(next)
+            res.append(node)
+
+        start = -1
+        for node in out_degree:
+            if out_degree[node] == in_degree[node] + 1:
+                start = node
                 break
-            new_curr: int = cr[curr].pop()
-            right.append([curr, new_curr])
-            if not cr[curr]:
-                del cr[curr]
-            curr = new_curr
-        return right
+
+        if start == -1:
+            start = pairs[0][0]
+
+        visit(start)
+
+        res.reverse()
+
+        return [
+            [res[i-1], res[i]] 
+            for i in range(1, len(res))
+        ]
 
 
 def main() -> None:
