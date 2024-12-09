07d39efd90ab25040427c2d432cf9a2b5b683001
wrnlb666 <wrnlb666@gmail.com>
2024-12-09 23:17:33 +1100

    3152: py3 - attempt 2

--- 3152/py/test.py
+++ 3152/py/test.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 
 class Solution:
@@ -14,24 +14,55 @@ class Solution:
                 interval.append(True)
             last = curr
 
-        res: List[bool] = list()
-        for q in queries:
-            temp: bool = True
-            if q[0] == q[1]:
-                res.append(temp)
-                continue
-            for i in range(q[0], q[1]):
-                temp &= interval[i]
-                if not temp:
+        table: List[Tuple[int, int]] = list()
+        left: int = 0
+        for right in range(len(interval)):
+            if not interval[right]:
+                if left != right:
+                    table.append((left, right))
+                left = right + 1
+        if left != len(interval):
+            table.append((left, len(interval)))
+
+        def check(s: int, e: int) -> bool:
+            if not table:
+                return False
+
+            left: int = 0
+            right: int = len(table) - 1
+            index: int = len(table) - 1
+
+            while left <= right:
+                mid: int = (left + right) // 2
+                if table[mid][0] == s:
+                    index = mid
                     break
-            res.append(temp)
+                elif table[mid][0] > s:
+                    right = mid - 1
+                else:
+                    index = mid
+                    left = mid + 1
+
+            if s >= table[index][0] and e <= table[index][1]:
+                return True
+            return False
+
+        res: List[bool] = list()
+        for s, e in queries:
+            if s == e:
+                res.append(True)
+                continue
+            if check(s, e):
+                res.append(True)
+            else:
+                res.append(False)
 
         return res
 
 
 def main() -> None:
-    nums: List[int] = [3,4,1,2,6]
-    queries: List[List[int]] = [[0,4]]
+    nums: List[int] = [2,2,3,6,8,7,4,9]
+    queries: List[List[int]] = [[2,3]]
 
     res: List[bool] = Solution().isArraySpecial(nums, queries)
     print(res)
