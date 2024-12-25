4fe8568d4bbbf2bb675339032df69ba195f1f080
wrnlb666 <wrnlb666@gmail.com>
2024-12-25 22:02:34 +1100

    515: py3 - attempt 1

---
+++ 515/py/test.py
@@ -0,0 +1,101 @@
+from typing import Optional, Self, List, Deque
+from collections import deque
+
+
+null: None = None
+class TreeNode:
+    def __init__(self,
+                 val: int = 0,
+                 left: Optional[Self] = None,
+                 right: Optional[Self] = None,
+                 ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __repr__(self) -> str:
+        res: List[int | str] = []
+        nodes: Deque[Optional[Self]] = deque()
+        nodes.append(self)
+        while len(nodes) != 0:
+            curr: Optional[Self] = nodes.popleft()
+            if curr is None:
+                res.append("null")
+                continue
+            res.append(curr.val)
+            if curr.left is not None:
+                nodes.append(curr.left)
+                if curr.right is None:
+                    nodes.append(None)
+            if curr.right is not None:
+                if curr.left is None:
+                    nodes.append(None)
+                nodes.append(curr.right)
+        while res[-1] == "null":
+            res.pop(-1)
+        return str(res)
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    @classmethod
+    def from_list(cls, li: List[Optional[int]]) -> Self:
+        first: Optional[int] = li.pop(0)
+        if not first:
+            return cls()
+        root: Self = cls(first)
+        queue: Deque[Self] = deque([root])
+        while li:
+            left: Optional[int] = li.pop(0)
+            right: Optional[int] = None
+            if li:
+                right = li.pop(0)
+            parent: Self = queue.popleft()
+
+            if left:
+                node: Self = cls(left)
+                parent.left = node
+                queue.append(node)
+            if right:
+                node: Self = cls(right)
+                parent.right = node
+                queue.append(node)
+        return root
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        res: List[int] = list()
+        if root is None:
+            return res
+        queue: Deque[TreeNode] = deque([root])
+        while queue:
+            row: List[int] = list()
+            size: int = len(queue)
+            for _ in range(size):
+                node: TreeNode = queue.popleft()
+                row.append(node.val)
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+            res.append(max(row))
+
+        return res
+
+
+def main() -> None:
+    root: List[Optional[int]] = [1,3,2,5,3,null,9]
+    res: List[int] = Solution().largestValues(TreeNode.from_list(root))
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
