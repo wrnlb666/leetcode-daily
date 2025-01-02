6934c34e04ddf2eaf7a04bb8dcf88478816d39ff
wrnlb666 <wrnlb666@gmail.com>
2025-01-03 01:42:26 +1100

    2559: py3 - attempt 2

--- 2559/py/test.py
+++ 2559/py/test.py
@@ -4,38 +4,16 @@ from typing import List, Set
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
         vowels: Set[str] = {"a", "e", "i", "o", "u"}
-        targets: List[int] = list()
-        for i, w in enumerate(words):
+        prefix: List[int] = [0]
+        curr: int = 0
+        for w in words:
             if w[0] in vowels and w[-1] in vowels:
-                targets.append(i)
-
-        def bs(targets: List[int], n: int) -> int:
-            left: int = 0
-            right: int = len(targets) - 1
-            res: int = -1
-            while left <= right:
-                mid: int = (left + right) // 2
-                if n < targets[mid]:
-                    res = mid
-                    right = mid - 1
-                elif n > targets[mid]:
-                    left = mid + 1
-                else:
-                    return mid
-            return res
+                curr += 1
+            prefix.append(curr)
 
         res: List[int] = list()
         for s, e in queries:
-            i: int = bs(targets, s)
-            curr: int = 0
-            if i == -1:
-                res.append(curr)
-                continue
-            while i < len(targets) and targets[i] <= e:
-                curr += 1
-                i += 1
-            res.append(curr)
-
+            res.append(prefix[e+1] - prefix[s])
         return res
 
 
