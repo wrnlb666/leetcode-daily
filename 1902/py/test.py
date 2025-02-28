f1a3b43f0bef3c0c0e544cecd1a1ded5089396dc
wrnlb666 <wrnlb666@gmail.com>
2025-03-01 03:58:05 +1100

    1902: py3 - attempt 2 (copied from edtorial)

--- 1902/py/test.py
+++ 1902/py/test.py
@@ -1,33 +1,33 @@
-from typing import Dict, Tuple
+from typing import List
 
 
 class Solution:
-    def shortestCommonSupersequence(
-        self, str1: str, str2: str, cache: Dict[Tuple[str, str], str] = dict()
-    ) -> str:
-        target: Tuple[str, str] = (str1, str2)
-        if target in cache:
-            return cache[target]
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        n1 = len(str1)
+        n2 = len(str2)
 
-        if not str1 and not str2:
-            return ""
+        prev: List[str] = [str2[0:col] for col in range(n2 + 1)]
 
-        if not str1:
-            return str2
-        if not str2:
-            return str1
+        for row in range(1, n1 + 1):
+            curr: List[str] = [str1[0:row]] + ["" for _ in range(n2)]
 
-        if str1[0] == str2[0]:
-            cache[target] = str1[0] + self.shortestCommonSupersequence(
-                str1[1:], str2[1:]
-            )
-            return cache[target]
+            for col in range(1, n2 + 1):
+                if str1[row - 1] == str2[col - 1]:
+                    curr[col] = prev[col - 1] + str1[row - 1]
+                else:
+                    res1: str = prev[col]
 
-        res1 = str1[0] + self.shortestCommonSupersequence(str1[1:], str2)
-        res2 = str2[0] + self.shortestCommonSupersequence(str1, str2[1:])
+                    res2: str = curr[col - 1]
 
-        cache[target] = res1 if len(res1) < len(res2) else res2
-        return cache[target]
+                    curr[col] = (
+                        res1 + str1[row - 1]
+                        if len(res1) < len(res2)
+                        else res2 + str2[col - 1]
+                    )
+
+            prev = curr
+
+        return prev[n2]
 
 
 def main() -> None:
