a3f260db8cfd3f09e5eba40e55de762f3b3dbf1e
wrnlb666 <wrnlb666@gmail.com>
2025-03-13 21:09:19 +1100

    3356: py3 - attempt 3

--- 3356/py/test.py
+++ 3356/py/test.py
@@ -3,16 +3,36 @@ from typing import List
 
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
-        res: int = 0
-        if not any(nums):
-            return res
-        for s, e, v in queries:
-            for i in range(s, e+1):
-                nums[i] -= min(v, nums[i])
-            res += 1
-            if not any(nums):
-                return res
-        return -1
+        left, right = 0, len(queries)
+
+        if not self._valid(nums, queries, right):
+            return -1
+
+        while left <= right:
+            middle = left + (right - left) // 2
+            if self._valid(nums, queries, middle):
+                right = middle - 1
+            else:
+                left = middle + 1
+
+        return left
+
+    def _valid(self, nums: List[int], queries: List[List[int]], k: int) -> bool:
+        n: int = len(nums)
+        total: int = 0
+        cache: List[int] = [0] * (n + 1)
+
+        for i in range(k):
+            start, end, val = queries[i]
+
+            cache[start] += val
+            cache[end + 1] -= val
+
+        for i in range(n):
+            total += cache[i]
+            if total < nums[i]:
+                return False
+        return True
 
 
 def main() -> None:
