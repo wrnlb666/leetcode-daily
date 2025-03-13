5ba3f1ae48881140346cc7bd8ff0ecdd947964b7
wrnlb666 <wrnlb666@gmail.com>
2025-03-13 21:02:44 +1100

    3356: py3 - attempt 1 (TLE)

---
+++ 3356/py/test.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        res: int = 0
+        for n in nums:
+            if n > 0:
+                break
+        else:
+            return res
+        for s, e, v in queries:
+            for i in range(s, e+1):
+                nums[i] -= v
+            res += 1
+            for n in nums:
+                if n > 0:
+                    break
+            else:
+                return res
+        return -1
+
+
+def main() -> None:
+    nums: List[int] = [2, 0, 2]
+    queries: List[List[int]] = [[0, 2, 1], [0, 2, 1], [1, 1, 3]]
+    res: int = Solution().minZeroArray(nums, queries)
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
